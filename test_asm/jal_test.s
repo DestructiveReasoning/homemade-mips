13fcd3e5e2a9e25a977f57cc9067fd55dc7abb00
DestructiveReasoning <harleyw@hotmail.com>
2018-03-27 21:24:48 -0400

    Includes a few assembly programs for testing the CPU

---
+++ test_asm/jal_test.s
@@ -0,0 +1,25 @@
+	.globl main
+	.text
+
+main:
+		addi $r5, $r0, #16
+		jal addone
+		addi $r5, $r5, #-1
+		jal bs
+		addi $r6, $r0, #1
+		jal inter
+ret:	add $r5, $r5, $r6
+
+addone:
+		addi $r5, $r5, #1
+		jr $ra
+
+bs:
+		jr $ra
+
+inter:
+		jal end
+		j ret
+
+end:
+		jr $ra
