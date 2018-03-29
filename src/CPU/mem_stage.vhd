8245fe2be34e3eb343a3b57edf20d2d6c51b7b3a
Alex <prizestufff@gmail.com>
2018-03-29 15:33:06 -0400

    Add files via upload

--- src/CPU/mem_stage.vhd
+++ src/CPU/mem_stage.vhd
@@ -14,14 +14,27 @@ END mem_stage;
 
 ARCHITECTURE memory OF mem_stage IS
     -- TODO: Instantiate memory
-    
-    signal next_data, next_alu, next_address: STD_LOGIC_VECTOR(31 downto 0);
-    
+constant sz : integer := 127;
+type register_mem is array (sz downto 0) of std_logic_vector(31 downto 0);
+signal memory : register_mem := (others => (others => '0'));
+signal next_data, next_alu, next_address: STD_LOGIC_VECTOR(31 downto 0);
+signal i_addr : integer := to_integer(unsigned(addr));
+
+-- Value from ALU to store, then address from, open up internal memory and write to address then store data in that
+-- Include the part where it dumps to disk, include this from register vhd, logic to write to disk is there
+
 BEGIN
+
+   i_addr <= to_integer(unsigned(addr));
+
     process(clock)
     BEGIN
         if(rising_edge(clock)) then
-            -- TODO deal with memory
+		if(write = '1') then
+			memory(i_addr) <= write_data;
+		elsif(read = '1') then
+			output <= memory(i_addr);
+		end if;
         END IF;
     END process;
 END memory;
