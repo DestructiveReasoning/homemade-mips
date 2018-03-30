8ae71552e5059425d155c6ec1087412a234a53dc
Ali <ali.sharif2@mail.mcgill.ca>
2018-03-30 11:00:38 -0400

    tcl file for testing id_stage

---
+++ src/CPU/id_tb.tcl
@@ -0,0 +1,24 @@
+proc AddWaves {} {
+	;#Add waves we're interested in to the Wave window
+    add wave -position end sim:/id_tb/*
+}
+
+vlib work
+
+;# compile components
+vcom id_stage.vhd
+vcom id_tb.vhd
+vcom registerfile.vhd
+vcom ALUFunct_Encoder.vhd
+
+;# Start simulation
+vsim id_tb
+
+;# Generate a clock with 1ns period
+force -deposit clock 0 0 ns, 1 0.5 ns -repeat 1 ns
+
+;# Add the waves
+AddWaves
+
+;# Run for 100ns
+run 10ns
