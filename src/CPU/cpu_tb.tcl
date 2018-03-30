1c678cd55000f8788b2ce1d9eb71eeace0c297eb
Ali <ali.sharif2@mail.mcgill.ca>
2018-03-30 13:38:44 -0400

    tcl cpu new

--- src/CPU/cpu_tb.tcl
+++ src/CPU/cpu_tb.tcl
@@ -1,49 +1,13 @@
 proc AddWaves {} {
 	;#Add waves we're interested in to the Wave window
-  add wave -position end sim:/cpu_tb/clock
-  add wave -position end sim:/cpu_tb/stall
-  add wave -position end sim:/cpu_tb/if_instr_in
-  add wave -position end sim:/cpu_tb/if_newpc_in
-  add wave -position end sim:/cpu_tb/if_instr_out
-  add wave -position end sim:/cpu_tb/if_newpc_out
-  add wave -position end sim:/cpu_tb/the_new_addr
-  add wave -position end sim:/cpu_tb/id_instr_in
-  add wave -position end sim:/cpu_tb/id_newpc_in
-  add wave -position end sim:/cpu_tb/id_instr_out
-  add wave -position end sim:/cpu_tb/id_newpc_out
-  add wave -position end sim:/cpu_tb/id_dataa_in
-  add wave -position end sim:/cpu_tb/id_datab_in
-  add wave -position end sim:/cpu_tb/id_dataa_out
-  add wave -position end sim:/cpu_tb/id_datab_out
-  add wave -position end sim:/cpu_tb/id_imm_in
-  add wave -position end sim:/cpu_tb/id_imm_out
-  add wave -position end sim:/cpu_tb/id_ctrlsigs_in
-  add wave -position end sim:/cpu_tb/id_ctrlsigs_out
-  add wave -position end sim:/cpu_tb/ex_instr_in
-  add wave -position end sim:/cpu_tb/ex_instr_out
-  add wave -position end sim:/cpu_tb/ex_newpc_out
-  add wave -position end sim:/cpu_tb/ex_dataa_in
-  add wave -position end sim:/cpu_tb/ex_datab_in
-  add wave -position end sim:/cpu_tb/ex_dataa_out
-  add wave -position end sim:/cpu_tb/ex_datab_out
-  add wave -position end sim:/cpu_tb/ex_imm_out
-  add wave -position end sim:/cpu_tb/ex_ctrlsigs_out
-  add wave -position end sim:/cpu_tb/ex_alures
-  add wave -position end sim:/cpu_tb/mem_instr_out
-  add wave -position end sim:/cpu_tb/mem_newpc_out
-  add wave -position end sim:/cpu_tb/mem_dataa_in
-  add wave -position end sim:/cpu_tb/mem_datab_in
-  add wave -position end sim:/cpu_tb/mem_dataa_out
-  add wave -position end sim:/cpu_tb/mem_datab_out
-  add wave -position end sim:/cpu_tb/mem_imm_out
-  add wave -position end sim:/cpu_tb/mem_ctrlsigs_in
-  add wave -position end sim:/cpu_tb/mem_ctrlsigs_out
-  add wave -position end sim:/cpu_tb/wb_data
+  add wave -position end sim:/cpu_tb/*
+  add wave -position end sim:/testbench/*
 }                                           
 
 vlib work
 
 ;# Compile components
+vcom testbench.vhd
 vcom alu.vhd
 vcom alu_tb.vhd
 vcom alufunct_encoder.vhd
@@ -56,7 +20,7 @@ vcom registerfile.vhd
 vcom registerfile_tb.vhd
 
 ;# Start simulation
-vsim cpu_tb
+vsim testbench
 
 ;# Generate a clock with 1ns period
 force -deposit clock 0 0 ns, 1 0.5 ns -repeat 1 ns
