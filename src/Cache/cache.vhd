b1e83eba2b101c2a54b99300cf1716f7eb82b07f
DestructiveReasoning <harleyw@hotmail.com>
2018-04-09 22:39:35 -0400

    Implements circular queue eviction policy

--- src/Cache/cache.vhd
+++ src/Cache/cache.vhd
@@ -33,13 +33,16 @@ architecture arch of cache is
 
 -- declare signals here
 
+	CONSTANT SETS: INTEGER := (2**set_bits);
+
 	TYPE BLOCK_TYPE IS ARRAY((2**word_bits)-1 downto 0) OF STD_LOGIC_VECTOR(31 downto 0);
-	TYPE SET_TYPE IS ARRAY((2**set_bits)-1 downto 0) OF BLOCK_TYPE;
-	TYPE CACHE_TYPE IS ARRAY(32-(2**set_bits) downto 0) OF SET_TYPE;
-	TYPE TAG_SET_TYPE IS ARRAY(32-(2**set_bits) downto 0) OF STD_LOGIC_VECTOR(15-(word_bits+set_bits+2+1) downto 0);
-	TYPE TAG_TYPE IS ARRAY((2**set_bits)-1 downto 0) OF TAG_SET_TYPE;
+	TYPE SET_TYPE IS ARRAY(SETS-1 downto 0) OF BLOCK_TYPE;
+	TYPE CACHE_TYPE IS ARRAY(32-SETS downto 0) OF SET_TYPE;
+	TYPE TAG_SET_TYPE IS ARRAY(32-SETS downto 0) OF STD_LOGIC_VECTOR(15-(word_bits+set_bits+2+1) downto 0);
+	TYPE TAG_TYPE IS ARRAY(SETS-1 downto 0) OF TAG_SET_TYPE;
 	TYPE STATE_TYPE IS (POWERON, MEM_READ, MEM_WRITE, HIT, IDLE, CACHE_MISS);
-	TYPE VALIDS_TYPE IS ARRAY((2**set_bits)-1 downto 0) OF STD_LOGIC_VECTOR(32-(2**set_bits) downto 0);
+	TYPE VALIDS_TYPE IS ARRAY(SETS-1 downto 0) OF STD_LOGIC_VECTOR(32-SETS downto 0);
+	TYPE QUEUE_HEADS IS ARRAY(SETS-1 downto 0) OF INTEGER RANGE 0 TO (32-SETS);
 	SIGNAL valids: VALIDS_TYPE := (others => (others => '0'));					-- Stores the valid bit for each cache entry
 	SIGNAL dirty: VALIDS_TYPE := (others => (others => '0'));					-- Stores the valid bit for each cache entry
 	SIGNAL cache: CACHE_TYPE;
@@ -48,8 +51,9 @@ architecture arch of cache is
 	SIGNAL tag: STD_LOGIC_VECTOR(15-(word_bits+set_bits+2+1) downto 0);			-- Stores extracted tag from input address
 	SIGNAL word_offset: integer range 0 to (2**word_bits)-1;					-- Stores extracted word offset from input address
 	SIGNAL byte_offset: integer range 0 to 3;									-- Stores extracted byte offset from input address (should always be 0 if aligned)
-	SIGNAL index: integer range 0 to (2**set_bits)-1;							-- Stores extracted index from input address
+	SIGNAL index: integer range 0 to SETS-1;							-- Stores extracted index from input address
 	signal bin_idx : std_logic_vector(4 downto 0);
+	SIGNAL queues: QUEUE_HEADS := (others => 0);
 begin
 
 -- s_addr format (T - tag, I - index, W - word offset, B - byte offset):
@@ -73,7 +77,7 @@ begin
 					state <= IDLE;
 				WHEN IDLE => -- Main waiting state
 					if(s_read = '1') THEN
-						FOR i in 0 to (2**set_bits) - 1 LOOP
+						FOR i in 0 to SETS - 1 LOOP
 							if(tag = tags_vector(index)(i) and valids(index)(i) = '1') then
 								state <= HIT; -- Indicates that the cache's job is done
 							else 
@@ -83,7 +87,7 @@ begin
 						END LOOP;
 					elsif(s_write = '1') THEN
 						data := s_writedata;
-						FOR i in 0 to (2**set_bits) - 1 LOOP
+						FOR i in 0 to SETS - 1 LOOP
 							if(tag = tags_vector(index)(i) and valids(index)(i) = '1') then
 								cache(index)(i)(word_offset) <= data;
 								dirty(index)(i) <= '1';
@@ -99,14 +103,17 @@ begin
 					-- Upon cache miss, regardless of read or write, must check if block is dirty
 					-- If dirty, always write to memory
 					-- TODO: Implement more clever eviction policy
-					if(dirty(index)(0) = '1') then
+					IF 32-SETS /= 0 THEN
+						queues(index) <= (queues(index) + 1) rem (32-SETS);
+					END IF;
+					if(dirty(index)(queues(index)) = '1') then
 						state <= MEM_WRITE;
 					else
 						state <= MEM_READ;
 					end if;
 				WHEN HIT =>
 					s_readdata <= cache(index)(0)(word_offset);
-					FOR i IN 0 TO (32 - (2**set_bits)) LOOP
+					FOR i IN 0 TO (32 - SETS) LOOP
 						IF tag = tags_vector(index)(i) THEN
 							s_readdata <= cache(index)(i)(word_offset);
 						END IF;
@@ -117,19 +124,19 @@ begin
 					m_read <= '1';
 
 					--TODO: Implement more clever eviction policy
-					dirty(index)(0) <= '0'; -- Newly-read block is always clean
-					valids(index)(0) <= '1'; -- Newly-read block is always valid
-					tags_vector(index)(0) <= tag;
+					dirty(index)(queues(index)) <= '0'; -- Newly-read block is always clean
+					valids(index)(queues(index)) <= '1'; -- Newly-read block is always valid
+					tags_vector(index)(queues(index)) <= tag;
 					
 					if(m_waitrequest = '0') then
-						cache(index)(0)(word_offset)((byte_index + 1) * 8 - 1 downto byte_index * 8) <= m_readdata; -- Read appropriate byte from word
+						cache(index)(queues(index))(word_offset)((byte_index + 1) * 8 - 1 downto byte_index * 8) <= m_readdata; -- Read appropriate byte from word
 						byte_index := byte_index + 1;
 						m_read <= '0';
 						if(byte_index = 4) then
 							if(write_miss = '1') then
 								write_miss := '0';
-								cache(index)(0)(word_offset) <= data; -- When finished reading, write appropriate word in block if a write was requested
-								dirty(index)(0) <= '1';
+								cache(index)(queues(index))(word_offset) <= data; -- When finished reading, write appropriate word in block if a write was requested
+								dirty(index)(queues(index)) <= '1';
 							end if;
 							state <= HIT;
 							byte_index := 0;
@@ -149,8 +156,8 @@ begin
 					else
 --						m_addr <= to_integer(unsigned(tags_vector(index))) * 512 + index * 16 + word_offset*4 + byte_index;
 						--TODO: Implement more clever eviction policy
-						m_addr <= to_integer(unsigned(tags_vector(index)(0)))*(2**(2+word_bits+set_bits)) + index*(2**(2+word_bits)) + word_offset*4 + byte_index;
-						m_writedata <= cache(index)(0)(word_offset)((byte_index + 1) * 8 - 1 downto byte_index * 8);
+						m_addr <= to_integer(unsigned(tags_vector(index)(queues(index))))*(2**(2+word_bits+set_bits)) + index*(2**(2+word_bits)) + word_offset*4 + byte_index;
+						m_writedata <= cache(index)(queues(index))(word_offset)((byte_index + 1) * 8 - 1 downto byte_index * 8);
 						m_write <= '1';
 					end if;
 				WHEN others => null;
